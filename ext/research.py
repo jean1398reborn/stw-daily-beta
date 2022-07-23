eefefc2086dd6a770122acfb1978415b7fd42275
Jean Du Plessis <46666120+jean1398reborn@users.noreply.github.com>
2022-07-23 23:12:37 +1000

    Bug Fixes & Added Patented FORT Claiming System
    FIXED:
    - On the previous version i specified an unknown error relating to notifications, the reason for this error was that at maximum research points for some reason epic games decides to not add notifications, so i fixed it!
    
    FORT CLAIMING SYSTEM:
    The main feature of the research command is enabling you to claim your fort stats, currently the system is incomplete apart from the main user interface through the embed.  
    
    FORT Claiming System To Do:
     - Claim Stats
     - Update embed from claimed stats

--- ext/research.py
+++ ext/research.py
@@ -20,6 +20,30 @@ from discord.commands import (  # Importing the decorator that makes slash comma
     slash_command,
 )
 
+class ResearchView(discord.ui.View):
+
+    #creo kinda fire though ngl
+    def __init__(self, client):  
+        super().__init__()
+
+        # Create the different buttons for the different FORT stats <33
+        async def simple_callback(ctx):
+            await ctx.response.send_message("Beep Boop im a robot!")
+
+        button = discord.ui.Button(style=discord.ButtonStyle.success, emoji=client.config["emojis"]["fortitude"])
+        button.callback = simple_callback
+        self.add_item(button)
+        button = discord.ui.Button(style=discord.ButtonStyle.success, emoji=client.config["emojis"]["offense"])
+        button.callback = simple_callback
+        self.add_item(button)
+        button = discord.ui.Button(style=discord.ButtonStyle.success, emoji=client.config["emojis"]["resistance"])
+        button.callback = simple_callback
+        self.add_item(button)
+        button = discord.ui.Button(style=discord.ButtonStyle.success, emoji=client.config["emojis"]["tech"])
+        button.callback = simple_callback
+        self.add_item(button)
+
+    
 # cog for the research related commands.
 class Research(ext.Cog):
     
@@ -28,14 +52,18 @@ class Research(ext.Cog):
         self.emojis = client.config["emojis"]
         self.token_guid_research = "Token_collectionresource_nodegatetoken01"
         self.item_templateid_research = "Token:collectionresource_nodegatetoken01"
+        
     def check_for_research_points_item(self, query_json):
 
+
+        # Yes you can use the itemGuid from the notifications response from the claimcollectedresources response
+        # but, you do not get notifications when you are at maximum research points!
         items = query_json['profileChanges'][0]['profile']['items']
         
         for key, item in items.items():
             try:
                 if item['templateId'] == f"{self.item_templateid_research}":
-                    return item
+                    return item, key
             except: pass
 
         return None
@@ -64,13 +92,13 @@ class Research(ext.Cog):
         embed.add_field(name= "\u200B", value= "\u200B", inline=True)
         embed.add_field(name=f'**{self.client.config["emojis"]["resistance"]} Resistance: **',
             value=f'```{resistance}```\u200b', inline=True)
-        embed.add_field(name=f'**{self.client.config["emojis"]["tech"]} Tech: **',
+        embed.add_field(name=f'**{self.client.config["emojis"]["tech"]} Technology: **',
             value=f'```{tech}```\u200b', inline=True)
         return embed
     
     async def research_command(self, ctx, slash, authcode, auth_opt_out):
         error_colour = self.client.colours["error_red"]
-        succ_colour = self.client.colours["success_green"]
+        gren = self.client.colours["research_green"]
         yellow = self.client.colours["warning_yellow"]
         crown_yellow = self.client.colours["crown_yellow"]
         
@@ -106,11 +134,11 @@ class Research(ext.Cog):
 
         
         current_levels = json_response['profileChanges'][0]['profile']['stats']['attributes']['research_levels']
-
         if current_levels["offense"] + current_levels["fortitude"] + current_levels["resistance"] + current_levels["technology"] == 480:
             embed = discord.Embed(
                 title=await stw.add_emoji_title(self.client, "Max", "crown"),
-                description="""\u200bCongratulations, you have maximum FORT stats.\n\u200b""",
+                description="""\u200b
+                Congratulations, you have **maximum** FORT stats.\n\u200b""",
                 colour=crown_yellow
             )
             
@@ -147,49 +175,78 @@ class Research(ext.Cog):
 
         # Get total points
         total_points_check = await asyncio.gather(asyncio.to_thread(self.check_for_research_points_item, json_response))
-        print(total_points_check)
         if total_points_check[0] == None:
             error_code = "errors.stwdaily.failed_total_points"
             embed = await stw.post_error_possibilities(ctx, self.client, "research", acc_name, error_code, support_url)
             final_embeds.append(embed)
             await stw.slash_edit_original(auth_info[0], slash, final_embeds)
             return
-
-        total_points = total_points_check[0]
-            
-        # Get CollectedResourceResult feedback
-        print(json_response)
-        research_feedback, check = json_response["notifications"], False
         
-        for notification in research_feedback:
-            if notification["type"] == "collectedResourceResult":
-                research_feedback, check =notification, True
-                break
-            
-        if not check:
-            error_code = "errors.stwdaily.failed_get_collected_resource_type"
-            embed = await stw.post_error_possibilities(ctx, self.client, "research", acc_name, error_code, support_url)
-            final_embeds.append(embed)
-            await stw.slash_edit_original(auth_info[0], slash, final_embeds)
-            return
-            
-        available_research_items, check = research_feedback["loot"]["items"], False
-        for research_item in available_research_items:
-            try:
-                if research_item["itemType"] == self.item_templateid_research:
-                    research_item, check = research_item, True
+        total_points, rp_token_guid = total_points_check[0][0], total_points_check[0][1]
+        
+        # I do believe that after some testing if you are at le maximum research points
+        # you do not recieve notifications so this must be wrapped in a try statement
+        # assume that research points generated is none since it is at max!
+        research_points_claimed = None
+        try:
+            research_feedback, check = json_response["notifications"], False
+
+            for notification in research_feedback:
+                if notification["type"] == "collectedResourceResult":
+                    research_feedback, check =notification, True
                     break
-            except:
-                pass
+                
+            if not check:
+                error_code = "errors.stwdaily.failed_get_collected_resource_type"
+                embed = await stw.post_error_possibilities(ctx, self.client, "research", acc_name, error_code, support_url)
+                final_embeds.append(embed)
+                await stw.slash_edit_original(auth_info[0], slash, final_embeds)
+                return
+                
+            available_research_items, check = research_feedback["loot"]["items"], False
+            for research_item in available_research_items:
+                try:
+                    if research_item["itemType"] == self.item_templateid_research:
+                        research_item, check = research_item, True
+                        break
+                except:
+                    pass
 
-        if not check:
-            error_code = "errors.stwdaily.failed_get_collected_resource_item"
-            embed = await stw.post_error_possibilities(ctx, self.client, "research", acc_name, error_code, support_url)
-            final_embeds.append(embed)
-            await stw.slash_edit_original(auth_info[0], slash, final_embeds)
-            return
+            if not check:
+                error_code = "errors.stwdaily.failed_get_collected_resource_item"
+                embed = await stw.post_error_possibilities(ctx, self.client, "research", acc_name, error_code, support_url)
+                final_embeds.append(embed)
+                await stw.slash_edit_original(auth_info[0], slash, final_embeds)
+                return
+    
+            research_points_claimed = research_item['quantity']
+        except: pass
+
+        #Create the embed for displaying nyaa~
+
+        claimed_text = ""
         
-        await ctx.send(f"{research_item} {total_points} {current_levels}")
+        if research_points_claimed != None:
+            if research_points_claimed == 1: claimed_text = f"*Claimed **{research_points_claimed}** research point,*\n"
+            else: claimed_text = f"*Claimed **{research_points_claimed}** research points,*\n"
+        embed = discord.Embed(
+            title=await stw.add_emoji_title(self.client, "Research", "research_point"),
+            description=f"""\u200b
+            {claimed_text}You currently have **{total_points['quantity']}** research point{'s' if total_points['quantity'] > 1 else ''} available. \n\u200b""",
+            colour=gren
+        )
+        
+        await self.add_fort_fields(embed, current_levels)
+        embed = await stw.set_thumbnail(self.client, embed, "research")
+        embed = await stw.add_requested_footer(ctx, embed)
+        research_view = ResearchView(self.client)
+        
+        final_embeds.append(embed)
+        await stw.slash_edit_original(auth_info[0], slash, final_embeds, view=research_view)
+
+        
+        
+        await ctx.send(f"{research_points_claimed} {total_points} {current_levels}")
         
     @ext.command(name='research',
                 aliases=['res', 'rsearch', 'reach','rese','rse','reasearch','resaesaer'],
